36b2fdcdd03917d7262621c92b50ea17aba3846f
Hamayun Khan <hkhan.swa@gmail.com>
2019-04-07 14:07:30 +0500

    Datatable classes

---
+++ app/DataTables/GCategoryDataTable.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\DataTables;
+
+use App\Gcategory;
+use Yajra\DataTables\Services\DataTable;
+
+class GCategoryDataTable extends DataTable
+{
+    /**
+     * Build DataTable class.
+     *
+     * @param mixed $query Results from query() method.
+     * @return \Yajra\DataTables\DataTableAbstract
+     */
+    public function dataTable($query)
+    {
+        return datatables($query)
+            ->addColumn('action', 'admin.gcategoryaction');
+    }
+
+
+    /**
+     * Get query source of dataTable.
+     *
+     * @param \App\User $model
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function query(Gcategory $model)
+    {
+        return $model->newQuery()->select('id', 'name');
+    }
+
+    /**
+     * Optional method if you want to use html builder.
+     *
+     * @return \Yajra\DataTables\Html\Builder
+     */
+    public function html()
+    {
+        return $this->builder()
+            ->columns($this->getColumns())
+            ->minifiedAjax()
+            ->addAction(['width' => '80px'])
+            //->editColumn('id', 'ID: {{$id}}')
+            ->parameters([
+                'dom' => 'Bfrtip',
+                'buttons' => ['create', 'reset', 'reload'],
+            ]);
+    }
+
+    /**
+     * Get columns.
+     *
+     * @return array
+     */
+    protected function getColumns()
+    {
+        return [
+            'id',
+            'name'
+        ];
+    }
+
+    /**
+     * Get filename for export.
+     *
+     * @return string
+     */
+    protected function filename()
+    {
+        return 'GCategory_' . date('YmdHis');
+    }
+}
