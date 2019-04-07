5614de56f151b2d4bd3f0f437ed62162f57fe423
Hamayun Khan <hkhan.swa@gmail.com>
2019-04-07 23:18:15 +0500

    DataTable fix.

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Messages;
-use App\DataTables\MessagesDatatable;
+use App\DataTables\MessagesDataTable;
 
 
 class MessageController extends Controller
